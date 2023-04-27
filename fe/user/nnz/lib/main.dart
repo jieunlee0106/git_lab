7b4f460ce004791cd7b326a83cea1a8b8a60807f
JieunLee <jieun3181@gmail.com>
2023-04-27 17:02:57 +0900

    feat: home페이지 디자인 완성

--- fe/user/nnz/lib/main.dart
+++ fe/user/nnz/lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       initialBinding: InitBindings(),
-      initialRoute: "/sharingDetail",
+      initialRoute: "/home",
       getPages: [
         GetPage(
           name: "/",
--- fe/user/nnz/lib/src/pages/home/home.dart
+++ fe/user/nnz/lib/src/pages/home/home.dart
@@ -8,6 +8,8 @@ import 'package:nnz/src/components/home_page_form/hash_tag.dart';
 import 'package:nnz/src/components/gray_line_form/gray_line.dart';
 import 'package:nnz/src/components/home_page_form/share_text.dart';
 import 'package:nnz/src/components/home_page_form/home_share_list.dart';
+
+import 'package:nnz/src/components/icon_data.dart';
 import 'package:nnz/src/config/config.dart';
 
 import 'package:nnz/src/pages/category/concert.dart';
@@ -27,7 +29,7 @@ class Home extends StatelessWidget {
           backgroundColor: Colors.white,
           iconTheme: IconThemeData(color: Colors.black),
           leading: Icon(Icons.account_circle),
-          title: Center(child: Image.asset('assets/logo.png', width: 80)),
+          title: Center(child: Image.asset(ImagePath.logo, width: 80)),
           actions: [Icon(Icons.notifications)],
         ),
         body: SingleChildScrollView(
@@ -48,15 +50,15 @@ class Home extends StatelessWidget {
                   ),
                   items: [
                     HomeBanner(
-                      image: 'assets/banner1.png',
+                      image: ImagePath.banner1,
                       num: 1,
                     ),
                     HomeBanner(
-                      image: 'assets/banner2.png',
+                      image: ImagePath.banner2,
                       num: 1,
                     ),
                     HomeBanner(
-                      image: 'assets/banner3.png',
+                      image: ImagePath.banner3,
                       num: 1,
                     ),
                   ],
@@ -71,34 +73,34 @@ class Home extends StatelessWidget {
                     children: [
                       HomeCategory(
                           page: ConcertPage(),
-                          image: 'assets/concert.png',
+                          image: ImagePath.concert,
                           categoryName: '콘서트',
                           num: 1),
                       HomeCategory(
                           page: musicalPage(),
-                          image: 'assets/musical.png',
+                          image: ImagePath.musical,
                           categoryName: '뮤지컬',
                           num: 1),
                       HomeCategory(
                         page: stagePage(),
-                        image: 'assets/stage.png',
+                        image: ImagePath.stage,
                         categoryName: '연극',
                         num: 1,
                       ),
                       HomeCategory(
                         page: moviePage(),
-                        image: 'assets/movie.png',
+                        image: ImagePath.movie,
                         categoryName: '영화',
                         num: 1,
                       ),
                       HomeCategory(
                           page: sportsPage(),
-                          image: 'assets/sports.png',
+                          image: ImagePath.sports,
                           categoryName: '스포츠',
                           num: 1),
                       HomeCategory(
                           page: esportsPage(),
-                          image: 'assets/esports.png',
+                          image: ImagePath.esports,
                           categoryName: 'e스포츠',
                           num: 1),
                     ],
@@ -130,10 +132,15 @@ class Home extends StatelessWidget {
                 GrayLine(),
                 HomeShareText(
                     text: '즉시 줄서기 가능한 나눔',
-                    image: 'assets/movie.png',
+                    image: ImagePath.pin,
                     smallText: '근처에서 진행중인 나눔에 줄서기를 해보세요'),
-
-                // MyHomePage()
+                HomeShareList(),
+                GrayLine(),
+                HomeShareText(
+                    text: '인기 나눔',
+                    image: ImagePath.fire,
+                    smallText: '현재 가장 인기있는 나눔이에요'),
+                HomeShareList(),
                 // Add other widgets here...
               ],
             ),
