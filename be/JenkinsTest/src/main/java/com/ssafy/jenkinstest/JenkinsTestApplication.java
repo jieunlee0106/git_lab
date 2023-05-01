8f2c55d2909fcc88f928d66ec5c33bfc8b3d6e89
yeomyuri42@gmail.com <yeomyuri42@gmail.com>
2023-05-01 17:09:53 +0900

    FEAT: Selenium 크롬 웹드라이버 테스트

--- be/JenkinsTest/src/main/java/com/ssafy/jenkinstest/JenkinsTestApplication.java
+++ be/JenkinsTest/src/main/java/com/ssafy/jenkinstest/JenkinsTestApplication.java
@@ -1,5 +1,8 @@
 package com.ssafy.jenkinstest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,20 @@ public class JenkinsTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsTestApplication.class, args);
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        //크롬 드라이버 셋팅 (드라이버 설치한 경로 입력)
+
+        // webDriver 옵션 설정.
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
+        options.addArguments("--lang=ko");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.setCapability("ignoreProtectedModeSettings", true);
+        options.addArguments("--remote-allow-origins=*");
+
+        ChromeDriver driver = new ChromeDriver(options);
     }
 
 }
