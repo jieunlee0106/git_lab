e3f6579e99985c72130e6c921ce89e80b57cc493
jongseong <>
2023-05-02 07:02:24 +0900

    FEAT: 이메일, 닉네임 형식 검사 추가

--- be/user-service/src/main/java/nnz/userservice/exception/ErrorCode.java
+++ be/user-service/src/main/java/nnz/userservice/exception/ErrorCode.java
@@ -4,11 +4,14 @@ import io.github.eello.nnz.common.exception.AbstractErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RequiredArgsConstructor
 public enum ErrorCode implements AbstractErrorCode {
 
+    INVALID_NICKNAME_PATTERN("U003", "올바르지 않은 닉네임 형식입니다.", BAD_REQUEST),
+    INVALID_EMAIL_PATTERN("U005", "올바르지 이메일 형식입니다.", BAD_REQUEST),
     NOT_FOUND_VERIFY("U006", "존재하지 않는 인증정보입니다.", NOT_FOUND),
     ;
 
