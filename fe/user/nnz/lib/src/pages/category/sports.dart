ce8e0d861641f836003a4ecdbb74c91346a770c5
JieunLee <jieun3181@gmail.com>
2023-05-02 17:55:03 +0900

    feat: sport cartegory

--- fe/user/nnz/lib/src/pages/category/sports.dart
+++ fe/user/nnz/lib/src/pages/category/sports.dart
@@ -14,9 +14,9 @@ class _SportsPageState extends State<SportsPage> {
   String sportEvent = ImagePath.bsbB;
 
   final List<String> _images = [
-    'https://picsum.photos/id/237/200/300',
-    'https://picsum.photos/id/238/200/300',
-    'https://picsum.photos/id/239/200/300',
+    '축구',
+    '야구',
+    '농구',
   ];
 
   @override
@@ -30,75 +30,67 @@ class _SportsPageState extends State<SportsPage> {
         centerTitle: true,
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Expanded(
             child: Stack(
+              alignment: Alignment(0, -0.8),
               children: [
-                Positioned.fill(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: DecorationImage(
+                Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
                         image: AssetImage(sportEvent),
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned.fill(
-                  child: Container(
-                    color: Colors.black.withOpacity(0.3),
+                        alignment: Alignment.topCenter),
                   ),
                 ),
                 Container(
-                  height: 130,
-                  child: Center(
-                    child: CarouselSlider(
-                      options: CarouselOptions(
-                        height: 800,
-                        autoPlay: true,
-                        enlargeCenterPage: true,
-                        enableInfiniteScroll: true,
-                        viewportFraction: 1,
-                        onPageChanged: (int index, _) {
-                          setState(() {
-                            if (index == 0) {
-                              sportEvent = ImagePath.socB;
-                            } else if (index == 1) {
-                              sportEvent = ImagePath.bsbB;
-                            } else {
-                              sportEvent = ImagePath.bkbB;
-                            }
-                          });
-                        },
-                      ),
-                      items: _images.map((image) {
-                        return Builder(
-                          builder: (BuildContext context) {
-                            int index = _images.indexOf(image);
-                            String assetImage;
-
-                            if (index == 0) {
-                              assetImage = ImagePath.socB;
-                            } else if (index == 1) {
-                              assetImage = ImagePath.bsbB;
-                            } else {
-                              assetImage = ImagePath.bkbB;
-                            }
-                            return Container(
-                              width: MediaQuery.of(context).size.width,
-                              margin: EdgeInsets.symmetric(horizontal: 10.0),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                image: DecorationImage(
-                                  image: AssetImage(assetImage),
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            );
-                          },
-                        );
-                      }).toList(),
+                  height: 200,
+                  child: CarouselSlider(
+                    options: CarouselOptions(
+                      height: 800,
+                      autoPlay: true,
+                      enlargeCenterPage: true,
+                      enableInfiniteScroll: true,
+                      viewportFraction: 0.8,
+                      onPageChanged: (int index, _) {
+                        setState(() {
+                          if (index == 0) {
+                            sportEvent = ImagePath.socB;
+                          } else if (index == 1) {
+                            sportEvent = ImagePath.bsbB;
+                          } else {
+                            sportEvent = ImagePath.bkbB;
+                          }
+                        });
+                      },
                     ),
+                    items: _images.map((image) {
+                      return Builder(
+                        builder: (BuildContext context) {
+                          int index = _images.indexOf(image);
+                          String assetImage;
+
+                          if (index == 0) {
+                            assetImage = ImagePath.socB;
+                          } else if (index == 1) {
+                            assetImage = ImagePath.bsbB;
+                          } else {
+                            assetImage = ImagePath.bkbB;
+                          }
+                          return Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: EdgeInsets.symmetric(horizontal: 10.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                image: AssetImage(assetImage),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    }).toList(),
                   ),
                 ),
               ],
